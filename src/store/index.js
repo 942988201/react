592bc9bb5df61e6c0b55c0702d7cbd9a92c5a92b
wanglongyi <wanglongyi>
2021-07-15 14:10:06 +0800

    mnbvfghm

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
-import thunk from "redux-thunk"
+import thunk from "redux-thunk";
+import createSageMiddleware from "redux-saga";
+import saga from "./sagas.js"
+const sagaMiddleware = createSageMiddleware()
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ ?
     window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__({}) : compose;
-const enhancer = composeEnhancers(applyMiddleware(thunk))
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware))
 const store = createStore(
     reducer,
     enhancer
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
+sagaMiddleware.run(saga)
 export default store;
